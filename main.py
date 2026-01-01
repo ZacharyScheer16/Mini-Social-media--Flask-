a82e842f952c12ae8cb9976c95ed9dde545f272e
Zachary Scheer <zscheer00@gmail.com>
2026-01-01 12:00:11 -0700

    imported uvicron and changed if __name__ with uvicorn commands

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from fastapidemo!")
+import uvicorn
 
+if __name__ == "__main__":     #running on local host anyone on ip address can access    #port 8080 is access
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True) #reload good for debugging
 
-if __name__ == "__main__":
-    main()
+    
