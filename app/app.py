b2e0984c40298344d70ad826213c44b1b4c17c11
Zachary Scheer <zscheer00@gmail.com>
2026-01-01 22:22:36 -0700

    Changed id from string to int
    created get post by id(parameter int id)

--- app/app.py
+++ app/app.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-text_posts = {"1": {"title": "First Post", "content": "This is the first post."}
-              , "2": {"title": "Second Post", "content": "This is the second post."
+text_posts = {1: {"title": "First Post", "content": "This is the first post."}
+              , 2: {"title": "Second Post", "content": "This is the second post."
                     }
-              , "3": {"title": "Third Post", "content": "This is the third post."}
+              , 3: {"title": "Third Post", "content": "This is the third post."}
               }
 
 @app.get("/posts")
 def get_all_posts():
     return text_posts
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    return text_posts.get(id)
