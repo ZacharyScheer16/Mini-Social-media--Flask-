c89d317672d3ac309ad981c059a30dd7ce74c6b9
Zachary Scheer <zscheer00@gmail.com>
2026-01-02 16:32:21 -0700

    Test.db created automatically and app. py file upload creat synced with db.py

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Depends, Form
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,6 +12,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+'''
 text_posts = {
     1: {"title": "Logistics Route Optimization", "content": "How to use AI to find the shortest delivery paths."},
     2: {"title": "FastAPI Performance Tips", "content": "Using asynchronous endpoints for high-concurrency APIs."},
@@ -42,3 +44,41 @@ def create_post(post: PostCreate)-> PostResponse:
      new_post = {"title": post.title, "content": post.content}
      text_posts[max(text_posts.keys()) + 1] = new_post
      return new_post
+     
+     
+     ****demo code for in-memory storage
+     '''
+
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(),
+    session: AsyncSession = Depends(get_async_session)
+):
+    post = Post(
+        caption=caption,
+        url="dummyurl",
+        file_type='photo',
+        file_name="dummy name"
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
+
+@app.get("/feed")
+async def get_feed(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()] #result.all()
+    post_data = []
+
+    for post in posts:
+        post_data.append({
+            "id": str(post.id),
+            "caption": post.caption,
+            "url": post.url,
+            "file_type": post.file_type,
+            "file_name": post.file_name,
+            "created_at": post.created_at.isoformat()
+        })
+    return {"posts": post_data}
--- app/db.py
+++ app/db.py
@@ -19,6 +19,7 @@ class Post(Base):
     __tablename__ = "posts"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     caption = Column(Text)
+    url = Column(String) # <--- ADD THIS LINE
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
