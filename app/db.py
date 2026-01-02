7fcc107f95c41f2e6a232890d031a71308846106
Zachary Scheer <zscheer00@gmail.com>
2026-01-02 14:39:49 -0700

    created database schema

--- app/db.py
+++ app/db.py
@@ -1,4 +1,5 @@
 from collections.abc import AsyncGenerator
+from datetime import datetime
 import uuid
 from sqlalchemy import create_engine
 
@@ -6,3 +7,15 @@ from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase , relationship
+import datetime
+
+
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+class Post(DeclarativeBase):
+    __tablename__ = "posts"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    caption = Column(Text)
+    file_type = Column(String, nullable=False)
+    file_name = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
